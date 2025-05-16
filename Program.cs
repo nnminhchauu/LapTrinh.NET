e545a476b4b1a00a6dd4f3c8f183b5755804940e
trangxuan1710 <25A4042243@hvnh.edu.vn>
2025-05-16 22:39:08 +0700

    add

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace QuanLySachTruyen
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TrangChu());
+       
         }
     }
 }
--- Vipham.Designer.cs
+++ Vipham.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.datavipham = new System.Windows.Forms.DataGridView();
             this.btthem = new System.Windows.Forms.Button();
             this.btsua = new System.Windows.Forms.Button();
             this.btxoa = new System.Windows.Forms.Button();
@@ -39,7 +39,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.txtmavipham = new System.Windows.Forms.TextBox();
             this.txttenvipham = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.bthuy = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.datavipham)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -48,111 +49,131 @@
             this.label1.BackColor = System.Drawing.SystemColors.Control;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label1.Location = new System.Drawing.Point(172, 9);
+            this.label1.Location = new System.Drawing.Point(194, 11);
             this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(18, 16, 18, 16);
-            this.label1.Size = new System.Drawing.Size(393, 63);
+            this.label1.Padding = new System.Windows.Forms.Padding(20);
+            this.label1.Size = new System.Drawing.Size(460, 77);
             this.label1.TabIndex = 1;
             this.label1.Text = "HỆ THỐNG MƯỢN SÁCH ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 110);
+            this.label2.Location = new System.Drawing.Point(146, 138);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.Size = new System.Drawing.Size(89, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Mã vi phạm";
             // 
-            // dataGridView1
+            // datavipham
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(115, 214);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(518, 128);
-            this.dataGridView1.TabIndex = 3;
+            this.datavipham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datavipham.Location = new System.Drawing.Point(52, 268);
+            this.datavipham.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.datavipham.Name = "datavipham";
+            this.datavipham.RowHeadersWidth = 62;
+            this.datavipham.RowTemplate.Height = 28;
+            this.datavipham.Size = new System.Drawing.Size(660, 160);
+            this.datavipham.TabIndex = 3;
+            this.datavipham.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datavipham_CellContentClick);
             // 
             // btthem
             // 
-            this.btthem.Location = new System.Drawing.Point(113, 394);
+            this.btthem.Location = new System.Drawing.Point(35, 492);
             this.btthem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btthem.Name = "btthem";
-            this.btthem.Size = new System.Drawing.Size(71, 28);
+            this.btthem.Size = new System.Drawing.Size(80, 35);
             this.btthem.TabIndex = 4;
             this.btthem.Text = "Thêm";
             this.btthem.UseVisualStyleBackColor = true;
+            this.btthem.Click += new System.EventHandler(this.btthem_Click);
             // 
             // btsua
             // 
-            this.btsua.Location = new System.Drawing.Point(222, 394);
+            this.btsua.Location = new System.Drawing.Point(158, 492);
             this.btsua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btsua.Name = "btsua";
-            this.btsua.Size = new System.Drawing.Size(74, 28);
+            this.btsua.Size = new System.Drawing.Size(83, 35);
             this.btsua.TabIndex = 4;
             this.btsua.Text = "Sửa";
             this.btsua.UseVisualStyleBackColor = true;
+            this.btsua.Click += new System.EventHandler(this.btsua_Click);
             // 
             // btxoa
             // 
-            this.btxoa.Location = new System.Drawing.Point(330, 394);
+            this.btxoa.Location = new System.Drawing.Point(279, 492);
             this.btxoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btxoa.Name = "btxoa";
-            this.btxoa.Size = new System.Drawing.Size(69, 28);
+            this.btxoa.Size = new System.Drawing.Size(78, 35);
             this.btxoa.TabIndex = 4;
             this.btxoa.Text = "Xóa";
             this.btxoa.UseVisualStyleBackColor = true;
+            this.btxoa.Click += new System.EventHandler(this.btxoa_Click);
             // 
             // btluu
             // 
-            this.btluu.Location = new System.Drawing.Point(441, 394);
+            this.btluu.Location = new System.Drawing.Point(390, 492);
             this.btluu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btluu.Name = "btluu";
-            this.btluu.Size = new System.Drawing.Size(72, 28);
+            this.btluu.Size = new System.Drawing.Size(81, 35);
             this.btluu.TabIndex = 4;
             this.btluu.Text = "Lưu";
             this.btluu.UseVisualStyleBackColor = true;
+            this.btluu.Click += new System.EventHandler(this.btluu_Click);
             // 
             // btthoat
             // 
-            this.btthoat.Location = new System.Drawing.Point(555, 394);
+            this.btthoat.Location = new System.Drawing.Point(624, 492);
             this.btthoat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btthoat.Name = "btthoat";
-            this.btthoat.Size = new System.Drawing.Size(76, 28);
+            this.btthoat.Size = new System.Drawing.Size(86, 35);
             this.btthoat.TabIndex = 4;
             this.btthoat.Text = "Thoát";
             this.btthoat.UseVisualStyleBackColor = true;
+            this.btthoat.Click += new System.EventHandler(this.btthoat_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(130, 160);
+            this.label3.Location = new System.Drawing.Point(146, 200);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 16);
+            this.label3.Size = new System.Drawing.Size(94, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Tên vi phạm";
             // 
             // txtmavipham
             // 
-            this.txtmavipham.Location = new System.Drawing.Point(259, 104);
+            this.txtmavipham.Location = new System.Drawing.Point(291, 130);
+            this.txtmavipham.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtmavipham.Name = "txtmavipham";
-            this.txtmavipham.Size = new System.Drawing.Size(100, 22);
+            this.txtmavipham.ShortcutsEnabled = false;
+            this.txtmavipham.Size = new System.Drawing.Size(112, 26);
             this.txtmavipham.TabIndex = 5;
             // 
             // txttenvipham
             // 
-            this.txttenvipham.Location = new System.Drawing.Point(259, 154);
+            this.txttenvipham.Location = new System.Drawing.Point(291, 192);
+            this.txttenvipham.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txttenvipham.Name = "txttenvipham";
-            this.txttenvipham.Size = new System.Drawing.Size(100, 22);
+            this.txttenvipham.Size = new System.Drawing.Size(112, 26);
             this.txttenvipham.TabIndex = 5;
+            // 
+            // bthuy
+            // 
+            this.bthuy.Location = new System.Drawing.Point(512, 492);
+            this.bthuy.Name = "bthuy";
+            this.bthuy.Size = new System.Drawing.Size(85, 34);
+            this.bthuy.TabIndex = 6;
+            this.bthuy.Text = "Hủy";
+            this.bthuy.UseVisualStyleBackColor = true;
+            this.bthuy.Click += new System.EventHandler(this.bthuy_Click);
             // 
             // Vipham
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 446);
+            this.ClientSize = new System.Drawing.Size(800, 558);
+            this.Controls.Add(this.bthuy);
             this.Controls.Add(this.txttenvipham);
             this.Controls.Add(this.txtmavipham);
             this.Controls.Add(this.btthoat);
@@ -160,14 +181,15 @@
             this.Controls.Add(this.btxoa);
             this.Controls.Add(this.btsua);
             this.Controls.Add(this.btthem);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.datavipham);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Vipham";
             this.Text = "vipham";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.Vipham_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.datavipham)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,7 +199,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView datavipham;
         private System.Windows.Forms.Button btthem;
         private System.Windows.Forms.Button btsua;
         private System.Windows.Forms.Button btxoa;
@@ -186,5 +208,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtmavipham;
         private System.Windows.Forms.TextBox txttenvipham;
+        private System.Windows.Forms.Button bthuy;
     }
 }
